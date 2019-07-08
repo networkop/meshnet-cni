4518e7f8b87663338361ff3c755b143cc960071d
networkop <mmkashin@gmail.com>
2019-07-08 08:54:11 +0100

    Re-use existing config file on meshnet restart

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,9 +9,13 @@ if [ -d "/etc/cni/net.d/" ] && [ -f "./meshnet.conf" ]; then
   cp ./meshnet.conf /etc/cni/net.d/
 fi
 
-echo "Mergin existing CNI configuration with meshnet"
-existing=$(ls -1 /etc/cni/net.d/ | egrep "flannel|weave|bridge|calico|contiv|cilium|cni" | head -n1)
-jq  -s '.[1].delegate = (.[0].plugins[0])' /etc/cni/net.d/$existing /etc/cni/net.d/meshnet.conf | jq .[1] > /etc/cni/net.d/00-meshnet.conf
+if [ ! -f /etc/cni/net.d/00-meshnet.conf ]; then
+  echo "Mergin existing CNI configuration with meshnet"
+  existing=$(ls -1 /etc/cni/net.d/ | egrep "flannel|weave|bridge|calico|contiv|cilium|cni" | head -n1)
+  jq  -s '.[1].delegate = (.[0].plugins[0])' /etc/cni/net.d/$existing /etc/cni/net.d/meshnet.conf | jq .[1] > /etc/cni/net.d/00-meshnet.conf
+else
+  echo "Re-using existing CNI config"
+fi
 
 echo "Starting meshnetd daemon"
 /meshnetd
