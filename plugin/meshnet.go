780a53887b0c01be766462423008b279cbf15f00
Michael Kashin <mmkashin@gmail.com>
2021-09-19 16:44:37 +0100

    Replacing delegation with cni chaining; CNI installation is done by `meshnetd` (#30)
    * Replaces delegation with chaining
    * Moves CNI installation inside meshnetd's code
    
    Co-authored-by: Marcus Hines <52803648+mhines01@users.noreply.github.com>

--- plugin/meshnet.go
+++ plugin/meshnet.go
@@ -8,9 +8,9 @@ import (
 	"os"
 	"runtime"
 
-	"github.com/containernetworking/cni/pkg/invoke"
 	"github.com/containernetworking/cni/pkg/skel"
 	"github.com/containernetworking/cni/pkg/types"
+	"github.com/containernetworking/cni/pkg/types/current"
 	"github.com/containernetworking/cni/pkg/version"
 	"github.com/davecgh/go-spew/spew"
 	koko "github.com/redhat-nfvpe/koko/api"
@@ -49,12 +49,30 @@ func init() {
 }
 
 // loadConf loads information from cni.conf
-func loadConf(bytes []byte) (*netConf, error) {
+func loadConf(bytes []byte) (*netConf, *current.Result, error) {
 	n := &netConf{}
 	if err := json.Unmarshal(bytes, n); err != nil {
-		return nil, fmt.Errorf("failed to load netconf: %v", err)
+		return nil, nil, fmt.Errorf("failed to load netconf: %v", err)
 	}
-	return n, nil
+
+	// Parse previous result.
+	if n.RawPrevResult == nil {
+		// return early if there was no previous result, which is allowed for DEL calls
+		return n, &current.Result{}, nil
+	}
+
+	// Parse previous result.
+	var result *current.Result
+	var err error
+	if err = version.ParsePrevResult(&n.NetConf); err != nil {
+		return nil, nil, fmt.Errorf("could not parse prevResult: %v", err)
+	}
+
+	result, err = current.NewResultFromResult(n.PrevResult)
+	if err != nil {
+		return nil, nil, fmt.Errorf("could not convert result to current version: %v", err)
+	}
+	return n, result, nil
 }
 
 // getVxlanSource uses netlink to query the IP and LinkName of the interface with default route.
@@ -101,82 +119,23 @@ func makeVxlan(srcIntf string, peerIP string, idx int64) *koko.VxLan {
 	}
 }
 
-// DelegateAdd call
-func delegateAdd(ctx context.Context, netconf map[string]interface{}, intfName string) (result types.Result, err error) {
-	log.Infof("Calling delegateAdd for %s", netconf["type"].(string))
-
-	netconfBytes, err := json.Marshal(netconf)
-	if err != nil {
-		return nil, fmt.Errorf("failed serialising delegate config %v", err)
-	}
-
-	if os.Setenv("CNI_IFNAME", intfName) != nil {
-		return nil, fmt.Errorf("failed setting CNI_IFNAME")
-	}
-
-	log.Infof("About to delegate Add to %s", netconf["type"].(string))
-	result, err = invoke.DelegateAdd(ctx, netconf["type"].(string), netconfBytes, nil)
-	if err != nil {
-		return nil, fmt.Errorf("failed invoking Delegate Add %v", err)
-	}
-	return result, nil
-}
-
-// DelegateDel call
-func delegateDel(ctx context.Context, netconf map[string]interface{}, intfName string) error {
-	log.Infof("Calling delegateDel for %s", netconf["type"].(string))
-
-	netconfBytes, err := json.Marshal(netconf)
-	if err != nil {
-		return fmt.Errorf("failed serialising delegate config %v", err)
-	}
-	if os.Setenv("CNI_IFNAME", intfName) != nil {
-		return fmt.Errorf("failed setting CNI_IFNAME")
-	}
-	err = invoke.DelegateDel(ctx, netconf["type"].(string), netconfBytes, nil)
-	if err != nil {
-		return fmt.Errorf("failed invoking Delegate Del %v", err)
-	}
-	return nil
-}
-
 // Adds interfaces to a POD
 func cmdAdd(args *skel.CmdArgs) error {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	log.Info("Parsing cni .conf file")
-	n, err := loadConf(args.StdinData)
+	n, result, err := loadConf(args.StdinData)
 	if err != nil {
 		return err
 	}
 
 	log.Info("Parsing CNI_ARGS environment variable")
 	cniArgs := k8sArgs{}
-	if err = types.LoadArgs(args.Args, &cniArgs); err != nil {
+	if err := types.LoadArgs(args.Args, &cniArgs); err != nil {
 		return err
 	}
 	log.Infof("Processing ADD POD in namespace %s", cniArgs.K8S_POD_NAMESPACE)
-
-	// Verifying and calling delegateAdd for the master plugin
-	if n.Delegate == nil {
-		log.Infof("'delegate' is a required field in config, it should be the config of the main plugin to use")
-	}
-	r, err := delegateAdd(ctx, n.Delegate, args.IfName)
-	if err != nil {
-		log.Infof("'delegate' plugin failed: %s", err)
-		return err
-	}
-
-	log.Info("Master plugin has finished")
-	log.Infof("Master plugin result is %+v", r)
-
-	// Finding the source IP and interface for VXLAN VTEP
-	srcIP, srcIntf, err := getVxlanSource()
-	if err != nil {
-		return err
-	}
-	log.Infof("Default route is via %s@%s", srcIP, srcIntf)
 
 	log.Infof("Attempting to connect to local meshnet daemon")
 	conn, err := grpc.Dial(localDaemon, grpc.WithInsecure())
@@ -194,9 +153,16 @@ func cmdAdd(args *skel.CmdArgs) error {
 		KubeNs: string(cniArgs.K8S_POD_NAMESPACE),
 	})
 	if err != nil {
-		log.Info("pod topology was not found, skipping")
-		return r.Print()
+		log.Infof("Pod %s:%s was not a topology pod returning", string(cniArgs.K8S_POD_NAMESPACE), string(cniArgs.K8S_POD_NAME))
+		return types.PrintResult(result, n.CNIVersion)
 	}
+
+	// Finding the source IP and interface for VXLAN VTEP
+	srcIP, srcIntf, err := getVxlanSource()
+	if err != nil {
+		return err
+	}
+	log.Infof("Default route is via %s@%s", srcIP, srcIntf)
 
 	// Marking pod as "alive" by setting its srcIP and NetNS
 	localPod.NetNs = args.Netns
@@ -376,26 +342,26 @@ func cmdAdd(args *skel.CmdArgs) error {
 			}
 		}
 	}
-	log.Infof("Connected all links, exiting with result %+v", r)
-	return r.Print()
+
+	return types.PrintResult(result, n.CNIVersion)
 }
 
 // Deletes interfaces from a POD
 func cmdDel(args *skel.CmdArgs) error {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	// Parsing cni .conf file
-	n, err := loadConf(args.StdinData)
-	if err != nil {
-		return err
-	}
 
-	// Parsing CNI_ARGS environment variable
 	cniArgs := k8sArgs{}
-	if err = types.LoadArgs(args.Args, &cniArgs); err != nil {
+	if err := types.LoadArgs(args.Args, &cniArgs); err != nil {
 		return err
 	}
 	log.Infof("Processing DEL request: %s", cniArgs.K8S_POD_NAME)
+
+	log.Info("Parsing cni .conf file")
+	n, result, err := loadConf(args.StdinData)
+	if err != nil {
+		return err
+	}
 
 	conn, err := grpc.Dial(localDaemon, grpc.WithInsecure())
 	if err != nil {
@@ -411,71 +377,63 @@ func cmdDel(args *skel.CmdArgs) error {
 		Name:   string(cniArgs.K8S_POD_NAME),
 		KubeNs: string(cniArgs.K8S_POD_NAMESPACE),
 	})
-	if err == nil {
-		log.Infof("Topology data still exists in CRs, cleaning up it's status")
-		// By setting srcIP and NetNS to "" we're marking this POD as dead
-		localPod.NetNs = ""
-		localPod.SrcIp = ""
-		meshnetClient.SetAlive(ctx, localPod)
+	if err != nil {
+		log.Infof("Pod %s:%s is not topology returning", string(cniArgs.K8S_POD_NAMESPACE), string(cniArgs.K8S_POD_NAME))
+		return types.PrintResult(result, n.CNIVersion)
+	}
 
-		log.Infof("Iterating over each link for clean-up")
-		for _, link := range localPod.Links { // Iterate over each link of the local pod
+	log.Infof("Topology data still exists in CRs, cleaning up it's status")
+	// By setting srcIP and NetNS to "" we're marking this POD as dead
+	localPod.NetNs = ""
+	localPod.SrcIp = ""
+	meshnetClient.SetAlive(ctx, localPod)
 
-			// Creating koko's Veth struct for local intf
-			myVeth, err := makeVeth(args.Netns, link.LocalIntf, link.LocalIp)
-			if err != nil {
-				log.Infof("Failed to construct koko Veth struct")
-				return err
-			}
+	log.Infof("Iterating over each link for clean-up")
+	for _, link := range localPod.Links { // Iterate over each link of the local pod
 
-			log.Infof("Removing link %s", link.LocalIntf)
-			// API call to koko to remove local Veth link
-			if err = myVeth.RemoveVethLink(); err != nil {
-				// instead of failing, just log the error and move on
-				log.Infof("Error removing Veth link: %s", err)
-			}
+		// Creating koko's Veth struct for local intf
+		myVeth, err := makeVeth(args.Netns, link.LocalIntf, link.LocalIp)
+		if err != nil {
+			log.Infof("Failed to construct koko Veth struct")
+			return err
+		}
 
-			// Setting reversed skipped flag so that this pod will try to connect veth pair on restart
-			log.Infof("Setting skip-reverse flag on peer %s", link.PeerPod)
-			ok, err := meshnetClient.SkipReverse(ctx, &mpb.SkipQuery{
-				Pod:    localPod.Name,
-				Peer:   link.PeerPod,
-				KubeNs: string(cniArgs.K8S_POD_NAMESPACE),
-			})
-			if err != nil || !ok.Response {
-				log.Infof("Failed to set skip reversed flag on our peer %s", link.PeerPod)
-				return err
-			}
+		log.Infof("Removing link %s", link.LocalIntf)
+		// API call to koko to remove local Veth link
+		if err = myVeth.RemoveVethLink(); err != nil {
+			// instead of failing, just log the error and move on
+			log.Infof("Error removing Veth link: %s", err)
+		}
 
+		// Setting reversed skipped flag so that this pod will try to connect veth pair on restart
+		log.Infof("Setting skip-reverse flag on peer %s", link.PeerPod)
+		ok, err := meshnetClient.SkipReverse(ctx, &mpb.SkipQuery{
+			Pod:    localPod.Name,
+			Peer:   link.PeerPod,
+			KubeNs: string(cniArgs.K8S_POD_NAMESPACE),
+		})
+		if err != nil || !ok.Response {
+			log.Infof("Failed to set skip reversed flag on our peer %s", link.PeerPod)
+			return err
 		}
 	}
-
-	// Verifying and calling DelegateDel for the master plugin
-	if n.Delegate == nil {
-		return fmt.Errorf(`"delegate" is a required field in config, it should be the config of the main plugin to use`)
-	}
-	if err = delegateDel(context.Background(), n.Delegate, args.IfName); err != nil {
-		log.Infof("Delegate plugin failed to remove the interface")
-		return err
-	}
-
 	return nil
 }
 
 func main() {
-	fp, err := os.OpenFile("/var/log/meshnet-cni.log", os.O_APPEND+os.O_CREATE+os.O_RDWR, os.ModeAppend)
+	fp, err := os.OpenFile("/var/log/meshnet-cni.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
 	if err == nil {
 		log.SetOutput(fp)
 	}
+	retCode := 0
 	e := skel.PluginMainWithError(cmdAdd, cmdGet, cmdDel, version.All, "CNI plugin meshnet v0.3.0")
-	fp.Close()
 	if e != nil {
-		if err := e.Print(); err != nil {
-			log.Print("Error writing error JSON to stdout: ", err)
-			os.Exit(1)
-		}
+		log.Errorf("failed to run meshnet cni: %v", e.Print())
+		retCode = 1
 	}
-	os.Exit(0)
+	log.Infof("meshnet cni call successful")
+	fp.Close()
+	os.Exit(retCode)
 }
 
 func cmdGet(args *skel.CmdArgs) error {
