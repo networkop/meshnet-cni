ad317676229240839923fee1b5220f355a1b4252
Ashutosh Kumar <ashutshkumr@gmail.com>
2022-08-08 08:05:45 +0000

    fixed makefile and doc to allow make-install for both vxlan and grpc link

--- .mk/kustomize.mk
+++ .mk/kustomize.mk
@@ -11,16 +11,9 @@ kust-ensure:
 kustomize: kust-ensure
 	cd manifests/overlays/e2e && $(GOPATH)/bin/kustomize edit set image ${DOCKER_IMAGE}:${COMMIT}
 	cd -
-	cd manifests/overlays/grpc-link && $(GOPATH)/bin/kustomize edit set image ${DOCKER_IMAGE}:${COMMIT}
+	cd manifests/overlays/grpc-link-e2e && $(GOPATH)/bin/kustomize edit set image ${DOCKER_IMAGE}:${COMMIT}
 
 
 .PHONY: kustomize-kops
 kustomize-kops: kust-ensure 
 	kubectl apply -k manifests/overlays/kops/ 
-
-
-#-------- thel section below need to be removed before upstream-----------------------
-
-.PHONY: kustomize-gcp
-kustomize-gcp: kust-ensure
-	cd manifests/overlays/grpc-link-gcp && $(GOPATH)/bin/kustomize edit set image ${DOCKER_IMAGE}=${GCP_TAG} 
